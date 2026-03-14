59b7669e9066f67922697c905113f28feb6acc94
GirishTidke <girishtidke9@gmail.com>
2026-03-14 17:04:33 +0530

    updated api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 const API_BASE =
-  // import.meta.env.VITE_API_URL || "https://dhanwarsha.adonservice.in/api";
-  import.meta.env.VITE_API_URL || "http://localhost:8001/api";
+  import.meta.env.VITE_API_URL || "https://dhanwarsha.adonservice.in/api";
+  // import.meta.env.VITE_API_URL || "http://localhost:8001/api";
 
 async function request<T>(path: string, options: RequestInit = {}): Promise<T> {
   const token = localStorage.getItem("token");
